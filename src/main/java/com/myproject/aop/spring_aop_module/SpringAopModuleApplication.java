388321aee3e9ccb3857b610b5c2060a164ab7dfc
Aditi Mishra <aditimishra91924@gmail.com>
2025-02-05 09:04:10 +0530

    SpringAopModuleApplication implements CommandLineRunner to execute logic at startup

--- src/main/java/com/myproject/aop/spring_aop_module/SpringAopModuleApplication.java
+++ src/main/java/com/myproject/aop/spring_aop_module/SpringAopModuleApplication.java
@@ -1,13 +1,31 @@
 package com.myproject.aop.spring_aop_module;
 
+import com.myproject.aop.spring_aop_module.business.BusinessService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
-public class SpringAopModuleApplication {
+public class SpringAopModuleApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	private BusinessService businessService;
+
+	public SpringAopModuleApplication(BusinessService businessService1) {
+		this.businessService = businessService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAopModuleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Value returned is {}", businessService.calculateMax());
 	}
 
 }
