b0395b8a49c48caec54a02d4661a8bd577c82de9
mishraditi <aditimishra91924@gmail.com>
2025-02-18 14:47:46 +0530

    Exploring @AfterReturning, @AfterThrowing AOP Annotations

--- src/main/java/com/myproject/aop/spring_aop_module/aspects/LoggingAspect.java
+++ src/main/java/com/myproject/aop/spring_aop_module/aspects/LoggingAspect.java
@@ -1,10 +1,7 @@
     package com.myproject.aop.spring_aop_module.aspects;
 
     import org.aspectj.lang.JoinPoint;
-    import org.aspectj.lang.annotation.After;
-    import org.aspectj.lang.annotation.AfterThrowing;
-    import org.aspectj.lang.annotation.Aspect;
-    import org.aspectj.lang.annotation.Before;
+    import org.aspectj.lang.annotation.*;
     import org.slf4j.Logger;
     import org.slf4j.LoggerFactory;
     import org.springframework.context.annotation.Configuration;
@@ -23,6 +20,24 @@
         @After("execution(* com.myproject.aop.spring_aop_module.*.*.*(..))")
         public void logMethodCallAfterExecution(JoinPoint joinPoint) {
             logger.info("After Aspect - {} has executed",  joinPoint);
+        }
+        @AfterThrowing(
+                pointcut ="execution(* com.myproject.aop.spring_aop_module.business.*.*(..))",
+                throwing = "exception"
+        )
+        public void logMethodCallAfterException(JoinPoint joinPoint, Exception exception) {
+            logger.error("AfterThrowing Aspect - {} has thrown an exception {}"
+                    ,  joinPoint, exception);
+        }
+
+        @AfterReturning(
+                pointcut ="execution(* com.myproject.aop.spring_aop_module.business.*.*(..))",
+                returning = "resultValue"
+        )
+        public void logMethodCallAfterSuccessfulExecution(JoinPoint joinPoint,
+                                                          Object resultValue) {
+            logger.info("AfterReturning Aspect - {} has returned {}"
+                    ,  joinPoint, resultValue);
         }
 
     }
